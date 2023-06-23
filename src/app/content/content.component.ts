9510a626d9beda3c419c25e59987fa4f8b27caff
GoktugYmsk <gogtug033@outlook.com>
2023-06-23 12:06:51 +0300

    filtering done

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+  @Input() data: any[] = [];
+  categories: any;
+  filteredData: any[] = [];
+  searchInput: string = '';
+
+  constructor(private dataService: DataService) {
+    this.data = this.dataService.data;
+  }
+
+  ngOnInit() {
+    this.dataService.categories$.subscribe(categories => {
+      this.categories = categories;
+      this.filterData();
+    });
+  }
+
+ filterData() {
+  this.filteredData = this.data.filter(item => {
+    const hasCategory = this.hasCategory(item.category.toLowerCase());
+    const includesSearchInput = item.title.toLowerCase().includes(this.searchInput.toLowerCase());
+    return hasCategory && includesSearchInput;
+  });
+}
+
+  hasCategory(category: string): boolean {
+    return this.categories && this.categories.includes(category);
+  }
+  
 
 }
--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  @Input() data: any[] = [];
+  selectedCategory: string = '';
 
+  constructor(private dataService: DataService) {
+    this.data = this.dataService.data;
+  }
+
+  isDuplicateCategory(category: string, currentIndex: number): boolean {
+    for (let i = 0; i < currentIndex; i++) {
+      if (this.data[i].category === category) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  onCategoryClick(category: string): void {
+    this.selectedCategory = category;
+    this.dataService.onCategoryClick(category)
+    console.log(category);
+  }
+
+  get filteredData(): any[] {
+    if (this.selectedCategory) {
+      return this.data.filter(item => item.category === this.selectedCategory);
+    }
+    return this.data;
+  }
 }
