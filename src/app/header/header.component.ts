d85dcda04041f8f0e832b4c610a10ce7785fb311
GoktugYmsk <gogtug033@outlook.com>
2023-06-23 22:08:56 +0300

    header overview created

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  logo = 'https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg';
 }
