6597b2f71d70ec072e31cf376f9ead343d27b022
GoktugYmsk <gogtug033@outlook.com>
2023-06-27 10:54:43 +0300

    Added focus function to input

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { DataService } from '../data.service';
 
 @Component({
@@ -10,6 +10,7 @@ export class HeaderComponent {
   logo = 'https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg';
   searchInput: string = '';
   basketCount: number = 0;
+  @ViewChild('searchInputField', { static: false }) searchInputField!: ElementRef;
 
   constructor(private dataService: DataService) { }
 
@@ -19,13 +20,17 @@ export class HeaderComponent {
 
   ngOnInit() {
     this.dataService.basketCount$.subscribe(count => {
-      this.basketCount = count
-    })
+      this.basketCount = count;
+    });
   }
 
   openBasket(active: boolean) {
     this.dataService.setActiveValue(active);
   }
-  
 
+  focusSearchInput() {
+    setTimeout(() => {
+      this.searchInputField.nativeElement.focus();
+    }, 0);
+  }
 }
